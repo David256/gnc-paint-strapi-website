4e22b91a3706630a75f2a53b47ea4c9a6d6befdf
David <>
2024-10-20 02:31:48 -0500

    feat: add optional className to ServiceCard

--- front-end/src/components/ServiceCard.tsx
+++ front-end/src/components/ServiceCard.tsx
@@ -4,13 +4,18 @@ import { Service } from '../types'
 interface ServiceCardProps {
   index: number
   service: Service
+  className?: string
 }
 
 const ServiceCard: FunctionComponent<ServiceCardProps> = (props) => {
   const { index, service } = props
   return (
     <>
-      <div className="p-[50px] flex flex-col gap-6 rounded-[60px] bg-white w-[350px] h-[400px]">
+      <div
+        className={`p-[50px] flex flex-col gap-6 rounded-[60px] bg-white w-[350px] h-[400px] ${
+          props.className ?? ''
+        }`}
+      >
         <aside className="flex flex-row w-[60px] h-[60px] border">[icon]</aside>
         <div className="flex flex-col gap-[10px]">
           <header className="flex flex-row text-7xl leading-[72px]">
