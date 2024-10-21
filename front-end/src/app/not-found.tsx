7579cccecae5a989e4ee6de0fc7f604349f8b339
David <>
2024-10-21 04:53:50 -0500

    style: update colors and styling for 404 error page

--- front-end/src/app/not-found.tsx
+++ front-end/src/app/not-found.tsx
@@ -1,16 +1,12 @@
 'use client'
 
 import Header from '@/components/Header'
+import './styles/not-found.css'
 
 export default function Error() {
   return (
     <>
-      <div
-        className="flex flex-col"
-        style={{
-          backgroundColor: 'var(--light-color)',
-        }}
-      >
+      <div className="flex flex-col">
         <Header />
       </div>
     </>
