6e077c2062eaa68eedf2b79ddd3f656b37348a6d
David <>
2024-10-21 04:53:22 -0500

    refactor: remove color from Header component to inherit from parent

--- front-end/src/components/Header.tsx
+++ front-end/src/components/Header.tsx
@@ -4,12 +4,7 @@ import { FunctionComponent } from 'react'
 const Header: FunctionComponent = () => {
   return (
     <>
-      <div
-        className="content-sheet-x py-14"
-        style={{
-          backgroundColor: 'var(--light-color)',
-        }}
-      >
+      <div className="content-sheet-x py-14">
         <header className="flex flex-row">
           <div className="bg-white rounded-full p-2 px-5 w-[172px] h-[96px] flex flex-col items-center justify-center">
             <Image src="/logo.png" alt="" width={127} height={63} />
