03a64d82fd43b26f9e1e23a69e973e3a81c0e7e5
David <>
2024-10-21 05:53:19 -0500

    feat: define types for data from external backend

--- front-end/src/types.ts
+++ front-end/src/types.ts
@@ -1,4 +1,31 @@
+export interface ServiceCTO {
+  id: number
+  attributes: {
+    title: string
+    description: string
+  }
+}
+
+export interface ColorCTO {
+  id: number
+  attributes: {
+    name: string
+    hex: string
+    services: {
+      data: ServiceCTO[]
+    }
+  }
+}
+
 export interface Service {
+  id: number
   title: string
-  paragraph: string
+  description: string
+}
+
+export interface Color {
+  id: number
+  name: string
+  hex: string
+  services: Service[]
 }
