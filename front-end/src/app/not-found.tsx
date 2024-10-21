4562b0a03fa174f7eb71c07df6438a100f396497
David <>
2024-10-21 04:46:31 -0500

    feat: create 404 error page

---
+++ front-end/src/app/not-found.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import Header from '@/components/Header'
+
+export default function Error() {
+  return (
+    <>
+      <div
+        className="flex flex-col"
+        style={{
+          backgroundColor: 'var(--light-color)',
+        }}
+      >
+        <Header />
+      </div>
+    </>
+  )
+}
